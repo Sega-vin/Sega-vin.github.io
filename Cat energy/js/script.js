1f232cd2c9e5880d799ee75aed1fd42908265348
Sega-vin <56472271+Sega-vin@users.noreply.github.com>
2020-01-22 02:35:07 +0500

    Update script.js

--- Cat energy/js/script.js
+++ Cat energy/js/script.js
@@ -5,5 +5,5 @@ btn.addEventListener('click', function(){
 });
 window.onload = function(){
   toggled.classList.remove('main-nav--opened');
-  document.getElementById('iframe').src = 'https://vk.com/away.php?utf=1&to=https%3A%2F%2Fwww.google.com%2Fmaps%2Fembed%3Fpb%3D!1m16!1m12!1m3!1d23082.58473894339!2d40.20969743940372!3d43.68304601368505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z0LrRgNCw0YHQvdCw0Y8g0L_QvtC70Y_QvdCwINC-0YLQtdC70Lg!5e0!3m2!1sru!2sru!4v1579642136158!5m2!1sru!2sru';
+  document.getElementById('iframe').src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1998.6037872533248!2d30.32085871635935!3d59.93871648187619!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696310fca145cc1%3A0x42b32648d8238007!2z0JHQvtC70YzRiNCw0Y8g0JrQvtC90Y7RiNC10L3QvdCw0Y8g0YPQuy4sIDE5LzgsINCh0LDQvdC60YIt0J_QtdGC0LXRgNCx0YPRgNCzLCAxOTExODY!5e0!3m2!1sru!2sru!4v1579642474724!5m2!1sru!2sru";
 }
